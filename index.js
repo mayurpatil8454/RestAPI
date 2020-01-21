7f9e7b23497f30ee19106ac462f7647667fbdf2b
mayurpatil8454 <35427643+mayurpatil8454@users.noreply.github.com>
2020-01-21 11:13:04 +0530

    init

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/', function (req, res) {
   res.send('Welcome to REST service');  
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
